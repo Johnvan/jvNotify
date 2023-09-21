eed81d5b047997615e286c11691993cddc577bf4
Gerard van der Linden <gv@xnova.com>
2023-09-21 12:09:09 +1000

    Created empty .Net Maui project

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace jvNotify
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace jvNotify
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
